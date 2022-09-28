ff2600dbd53e7997e6113cf9d3e9ffb4df817032
ad105geppetto <ad105geppetto@gmail.com>
2022-09-29 04:33:22 +0900

    feat: 유저 컨트롤러 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { Users } from './users.model';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([Users]),
   ],
-  exports: [SequelizeModule]
+  exports: [SequelizeModule],
+  controllers: [UsersController]
 })
 export class UsersModule { }
