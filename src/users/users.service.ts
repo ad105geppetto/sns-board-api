ecc7405924fc56167f44d660581337c4a2fbc19f
ad105geppetto <ad105geppetto@gmail.com>
2022-09-29 04:40:57 +0900

    feat: 유저 회원가입 서비스 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Users } from './users.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(Users) private userModel: typeof Users,
+  ) { }
+
+  async create(userInfo) {
+    const user = await this.userModel.create(userInfo);
+    return user
+  }
+}
