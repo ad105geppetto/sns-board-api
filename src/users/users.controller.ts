82f4ca4713596610282ed6ee304a831a8a47b814
ad105geppetto <ad105geppetto@gmail.com>
2022-09-29 04:38:08 +0900

    feat: 유저 회원가입 컨트롤러 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('signup')
+export class UsersController {
+  constructor(private readonly UserService: UsersService) { }
+
+  @Post()
+  async createUser(@Body() userInfo) {
+    const user = await this.UserService.create(userInfo)
+    return user
+  }
+}
