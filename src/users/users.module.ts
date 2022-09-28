2d0c4a8508365ba44e13bef4358213c981a0c464
ad105geppetto <ad105geppetto@gmail.com>
2022-09-29 04:34:26 +0900

    feat: 유저 서비스 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,14 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { Users } from './users.model';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([Users]),
   ],
   exports: [SequelizeModule],
-  controllers: [UsersController]
+  controllers: [UsersController],
+  providers: [UsersService]
 })
 export class UsersModule { }
