f1affc7a3fba9237c17cfa596f40d34a400e5cd9
ad105geppetto <ad105geppetto@gmail.com>
2022-09-29 04:59:55 +0900

    feat: 유저 로그인 컨트롤러 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { Users } from './users.model';
 import { SignupController } from './controllers/signup.controller';
 import { SignupService } from './services/signup.service';
+import { LoginController } from './controllers/login.controller';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([Users]),
   ],
   exports: [SequelizeModule],
-  controllers: [SignupController],
+  controllers: [SignupController, LoginController],
   providers: [SignupService]
 })
 export class UsersModule { }
