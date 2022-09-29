c09ac475865d3cd1984125dbbf1349d6cd00c6f0
ad105geppetto <ad105geppetto@gmail.com>
2022-09-29 11:42:02 +0900

    feat: 게시글 서비스 추가

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,11 +4,13 @@ import { Boards } from './models/boards.model';
 import { BoardHashTags } from './models/board_hashTags.model';
 import { HashTags } from './models/hashTag.model';
 import { BoardsController } from './boards.controller';
+import { BoardsService } from './boards.service';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([Boards, HashTags, BoardHashTags])
   ],
-  controllers: [BoardsController]
+  controllers: [BoardsController],
+  providers: [BoardsService]
 })
 export class BoardsModule { }
