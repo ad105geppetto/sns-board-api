3cad7a8984bc742bbded3dfca12b6d9c88a3c4e4
ad105geppetto <ad105geppetto@gmail.com>
2022-09-29 11:41:13 +0900

    feat: 게시글 컨트롤러 추가

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,10 +3,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Boards } from './models/boards.model';
 import { BoardHashTags } from './models/board_hashTags.model';
 import { HashTags } from './models/hashTag.model';
+import { BoardsController } from './boards.controller';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([Boards, HashTags, BoardHashTags])
-  ]
+  ],
+  controllers: [BoardsController]
 })
 export class BoardsModule { }
