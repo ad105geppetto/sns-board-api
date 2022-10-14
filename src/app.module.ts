2d6875bc92f2b2146d51b9a5f2bbe86b3d70c81b
ad105geppetto <ad105geppetto@gmail.com>
2022-10-15 05:22:39 +0900

    style: typescript-eslint rule 수정
    # 참조
    - https://dev.to/msoup/fixing-require-statement-not-part-of-import-statementeslinttypescript-eslintno-var-requires-25n3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { UsersModule } from "./users/users.module";
