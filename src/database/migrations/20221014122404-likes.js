ba3adc0e1d3a6190cc3811b740bce32d043a1575
ad105geppetto <ad105geppetto@gmail.com>
2022-10-14 21:34:25 +0900

    feat: sequelize-cli migrations 사용하여 likes 테이블 추가

---
+++ src/database/migrations/20221014122404-likes.js
@@ -0,0 +1,47 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("likes", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      board_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "boards",
+          key: "id",
+        },
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    return await queryInterface.dropTable("likes");
+  },
+};
