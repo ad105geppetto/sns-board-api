ee77c9688ecc6bfdc4445206aee77dac8a3477bb
ad105geppetto <ad105geppetto@gmail.com>
2022-10-15 16:26:50 +0900

    fix: 유저 회원가입 수정
    # 내용
    - 동일한 아이디 입력시 데이터가 통과되었습니다.
    
    # 해결
    - 동일한 이메일 입력은 에러로 반환됩니다.

--- src/users/services/signup.service.ts
+++ src/users/services/signup.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 import { UserInfoDTO } from "../dto/userInfo.dto";
 import { Users } from "../entities/users.entity";
@@ -8,7 +8,15 @@ export class SignupService {
   constructor(@InjectModel(Users) private userModel: typeof Users) {}
 
   async create(userInfo: UserInfoDTO) {
-    const user = await this.userModel.create(userInfo);
-    return user;
+    const email = userInfo.email;
+    const [user, created] = await this.userModel.findOrCreate({
+      where: { email: email },
+    });
+
+    if (created) {
+      return user;
+    } else {
+      throw new BadRequestException("동일한 이메일이 존재합니다.");
+    }
   }
 }
